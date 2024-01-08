fe4689295f620ac739698a9110e861440cfdbe74
sysox <marek.sys@gmail.com>
2024-01-08 11:34:15 +0100

    added alphas 0.9, 0.99, ..., 1-10^-5 and 0.5

--- python/biased_data_params.py
+++ python/biased_data_params.py
@@ -6,12 +6,15 @@ def chi2_critical_value(alpha, num_bins = 256):
 block_bit_size = 8
 num_blocks = 400*1024*1024 / block_bit_size
 
-
 # print(chi2_critical_value(0.01, 2))
-alphas = [10**(-i) for i in range(1, 6)]
-chi2_critical_values = [chi2_critical_value(alpha) for alpha in alphas]
+alphas_small = {10**(-i):chi2_critical_value(10**(-i)) for i in range(1, 6)}
+alphas_large = {1-10**(-i): chi2_critical_value(1 - 10**(-i))  for i in range(1, 6)}
+alphas = {}
+alphas.update(alphas_small)
+alphas.update(alphas_large)
+alphas[0.5] = chi2_critical_value(0.5)
 
-num_swaps = [round(num_blocks*i/10) for i in range(11)]
+# num_swaps = [round(num_blocks*i/10) for i in range(11)]
 
-for chi2 in chi2_critical_values:
-    print(f"{round(chi2, 2)}")
+for alpha, chi2  in alphas.items():
+    print(f"alpha={alpha}:chi2={round(chi2, 2)} ")
